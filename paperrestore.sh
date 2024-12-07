4cec4b24e58c8a49fb6e11459ef17c2761c9f104
Jerry <57851723+SphenHe@users.noreply.github.com>
2024-12-07 19:13:19 +0800

    🐞 fix(path): commands update
    use command instead of path/to/command to add more capability

--- paperrestore.sh
+++ paperrestore.sh
@@ -11,13 +11,13 @@ if [ -z "$SCANNEDFILE" ]; then
     exit 1
 fi
 
-if ! [ -f "$SCANNEDFILE" ]; then
+if [ ! -f "$SCANNEDFILE" ]; then
     echo "$SCANNEDFILE is not a file"
     exit 1
 fi
 
-if [ ! -x "/usr/bin/zbarimg" ]; then
-    echo "/usr/bin/zbarimg missing"
+if [ ! -x "$(command -v zbarimg)" ]; then
+    echo "zbarimg missing"
     exit 2
 fi
 
@@ -27,7 +27,7 @@ fi
 # so convert that to \0<number><space>, so sort can sort on that
 # then remove all \n\0<number><space> so we get the originial without newlines added
 
-/usr/bin/zbarimg --raw -Sdisable -Sqrcode.enable "$SCANNEDFILE" \
+zbarimg --raw -Sdisable -Sqrcode.enable "$SCANNEDFILE" \
     | sed -e "s/\^/\x0/g" \
     | sort -z -n \
     | sed ':a;N;$!ba;s/\n\x0[0-9]* //g;s/\x0[0-9]* //g;s/\n\x0//g'
